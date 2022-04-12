4811239ea4bf10208203302cca0edd59ca776fce
qhchen <qhchen@secusoft.cc>
2022-04-12 17:43:04 +0800

    test

--- src/pages/template/one/index.tsx
+++ src/pages/template/one/index.tsx
@@ -3,8 +3,6 @@ import ContentWrapper from '@/components/ContentWrapper';
 import styles from './index.less';
 
 export default function Page() {
-  const [a, setA] = useState<string>(0);
-  const a = '';
-  a = 3;
+  const [a, setA] = useState<string>(04);
   return <ContentWrapper title="模版页面1">模版页面1</ContentWrapper>;
 }
