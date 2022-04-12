d6dd30090bbf8eee02388b9fa257ebdcd6d0a06d
qhchen <qhchen@secusoft.cc>
2022-04-12 17:28:51 +0800

    test

--- src/pages/template/one/index.tsx
+++ src/pages/template/one/index.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ContentWrapper from '@/components/ContentWrapper';
 import styles from './index.less';
 
 export default function Page() {
+  const [a, setA] = useState<string>(0);
   return <ContentWrapper title="模版页面1">模版页面1</ContentWrapper>;
 }
